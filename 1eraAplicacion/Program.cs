adda67ab2ae373656ad9cae8f803cd07de141190
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-03-31 23:00:00 -0300

    practicando constantes

--- 1eraAplicacion/Program.cs
+++ 1eraAplicacion/Program.cs
@@ -6,17 +6,11 @@ namespace _1eraAplicacion
     {
         static void Main(string[] args)
         {
+            const int VALOR = 5;
 
-            // Le pedimos al usuario que ingrese el 1er valor
-            Console.WriteLine("Introduce el primer número");
-            // Lo parseamos de texto a entero
-            int num1 = int.Parse(Console.ReadLine());
-            // Le pedimos al usuario que ingrese el 2do valor
-            Console.WriteLine("Introduce el segundo número");
-            // Lo parseamos de texto a entero
-            int num2 = int.Parse(Console.ReadLine());
-            // Realizamos la suma y mostramos el resultado
-            Console.WriteLine($"El resultado es {num1 + num2}");
+            const int VALOR2 = 7;
+
+            Console.WriteLine("El valor de la constante es: {1}", VALOR, VALOR2);
 
             // Espera a que el usuario presione una tecla antes de cerrar la consola
             Console.ReadKey();
